0fcf9783339c26d27220459c39939c06845109d0
Ian <helioblast86@gmail.com>
2016-04-09 17:37:01 -0500

    summands update

--- AlgorithmicToolBox/Week3/different_summands/different_summands.py
+++ AlgorithmicToolBox/Week3/different_summands/different_summands.py
@@ -2,9 +2,18 @@
 import sys
 
 def optimal_summands(n):
-    summands = []
-    #write your code here
+    summands = set()
+    if n == 1:
+        return set([1])
+    m = n
+    start = 1
+    while m-start not in summands and m-start > 0 and m-start != start:
+        summands.add(start)
+        m -= start
+        start += 1
+    summands.add(m)
     return summands
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
