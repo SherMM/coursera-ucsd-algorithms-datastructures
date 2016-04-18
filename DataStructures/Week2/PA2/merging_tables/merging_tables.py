1c6c3bef9d0086300080bdc6c3447619831d2bba
Shermm <helioblast86@gmail.com>
2016-04-18 15:48:44 -0500

    working on merge tables. sucks

--- DataStructures/Week2/PA2/merging_tables/merging_tables.py
+++ DataStructures/Week2/PA2/merging_tables/merging_tables.py
@@ -10,22 +10,31 @@ ans = max(lines)
 
 def getParent(table):
     # find parent and compress path
+    if table != parent[table]
+        parent[table] = getParent(parent[table])
     return parent[table]
 
 def merge(destination, source):
-    global ans
     realDestination, realSource = getParent(destination), getParent(source)
+
     if realDestination == realSource:
         return False
-    lines[realDestination] += lines[realSource]
-    lines[realSource] = 0
-    parent[realSource] = realDestination
-    ans = max(lines)
+
+    if rank[realDestination] > rank[realSource]:
+        # merge source into destination table
+        lines[realDestination] += lines[realSource]
+        lines[realDestination] = 0
+        parent[realSource] = realDestination
+    else:
+        # merge destination into source table
+        lines[realSource] += lines[realDestination]
+        lines[realDestination] = 0
+        parent[realDestination] = realSource
+        if rank[realDestination] == rank[realSource]:
+            rank[realSource] += 1
     return True
 
 for i in range(m):
-    #print(parent)
-    #print(lines)
     destination, source = map(int, sys.stdin.readline().split())
     merge(destination - 1, source - 1)
     print(ans)
