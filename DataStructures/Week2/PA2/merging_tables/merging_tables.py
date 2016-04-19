1b76a6ea90c6e729b3740552d2ad721b85f2069f
Shermm <helioblast86@gmail.com>
2016-04-18 20:49:08 -0500

    finally solved merge tables

--- DataStructures/Week2/PA2/merging_tables/merging_tables.py
+++ DataStructures/Week2/PA2/merging_tables/merging_tables.py
@@ -1,6 +1,7 @@
 # python3
 
 import sys
+import random
 
 n, m = map(int, sys.stdin.readline().split())
 lines = list(map(int, sys.stdin.readline().split()))
@@ -10,26 +11,31 @@ ans = max(lines)
 
 def getParent(table):
     # find parent and compress path
-    if table != parent[table]
+    if table != parent[table]:
         parent[table] = getParent(parent[table])
     return parent[table]
 
 def merge(destination, source):
+    global ans
     realDestination, realSource = getParent(destination), getParent(source)
 
     if realDestination == realSource:
         return False
 
     if rank[realDestination] > rank[realSource]:
-        # merge source into destination table
-        lines[realDestination] += lines[realSource]
-        lines[realDestination] = 0
+        # merge source table into destination table
         parent[realSource] = realDestination
+        lines[realDestination] += lines[realSource]
+        lines[realSource] += 0
+        if lines[realDestination] > ans:
+            ans = lines[realDestination]
     else:
         # merge destination into source table
+        parent[realDestination] = realSource
         lines[realSource] += lines[realDestination]
         lines[realDestination] = 0
-        parent[realDestination] = realSource
+        if lines[realSource] > ans:
+            ans = lines[realSource]
         if rank[realDestination] == rank[realSource]:
             rank[realSource] += 1
     return True
