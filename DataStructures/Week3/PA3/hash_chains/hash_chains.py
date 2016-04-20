4158a61aeb4686b16413797b3a711e98b3e93f78
Shermm <helioblast86@gmail.com>
2016-04-19 23:16:04 -0500

    finished hash table stuff

--- DataStructures/Week3/PA3/hash_chains/hash_chains.py
+++ DataStructures/Week3/PA3/hash_chains/hash_chains.py
@@ -1,4 +1,5 @@
 # python3
+from collections import deque
 
 class Query:
 
@@ -16,8 +17,7 @@ class QueryProcessor:
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        # store all strings in one list
-        self.elems = []
+        self.elems = [deque() for _ in range(self.bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -37,21 +37,22 @@ class QueryProcessor:
     def process_query(self, query):
         if query.type == "check":
             # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
-                        if self._hash_func(cur) == query.ind)
+            self.write_chain(self.elems[query.ind])
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
+            bucket = self._hash_func(query.s)
+            ind = -1 # assume won't be found
+            for index, item in enumerate(self.elems[bucket]):
+                if item == query.s:
+                    ind = index
+                    break
             if query.type == 'find':
                 self.write_search_result(ind != -1)
             elif query.type == 'add':
                 if ind == -1:
-                    self.elems.append(query.s)
+                    self.elems[bucket].appendleft(query.s)
             else:
                 if ind != -1:
-                    self.elems.pop(ind)
+                    self.elems[bucket].remove(query.s)
 
     def process_queries(self):
         n = int(input())
--- DataStructures/Week3/PA3/hash_substring/hash_substring.py
+++ DataStructures/Week3/PA3/hash_substring/hash_substring.py
@@ -1,4 +1,6 @@
 # python3
+import string
+import random
 
 def read_input():
     return (input().rstrip(), input().rstrip())
@@ -6,13 +8,47 @@ def read_input():
 def print_occurrences(output):
     print(' '.join(map(str, output)))
 
+def precompute_hashes(text, psize, p, x):
+    hashes = [0] * (len(text) - psize + 1)
+    s = text[len(text)-psize: len(text)]
+    hashes[len(text)-psize] = poly_hash(s, p, x)
+    y = 1
+    for i in range(0, psize):
+        y = (y * x) % p
+    for i in range(len(text)-psize-1, -1, -1):
+        hashes[i] = (x * hashes[i+1] + ord(text[i]) - y * ord(text[i+psize])) % p
+    return hashes
+
+def poly_hash(pattern, p, x):
+    h_val = 0
+    for i in range(len(pattern)-1, -1, -1):
+        h_val = (h_val * x + ord(pattern[i])) % p
+    return h_val
+
 def get_occurrences(pattern, text):
-    return [
-        i 
-        for i in range(len(text) - len(pattern) + 1) 
-        if text[i:i + len(pattern)] == pattern
-    ]
+    p = 1000000007
+    x = random.randrange(1, p)
+    phash = poly_hash(pattern, p, x)
+    hashes = precompute_hashes(text, len(pattern), p, x)
+    indexes = []
+    for i in range(len(hashes)):
+        if phash == hashes[i]:
+            indexes.append(i)
+    return indexes
+
+def test():
+    alpha = string.ascii_lowercase
+    n = 5*(10**5)
+    p = 100000
+    patt = [random.choice(alpha) for _ in range(p)]
+    text = [random.choice(alpha) for _ in range(n)]
+    text[50:50+p] = patt
+    patt = ''.join(patt)
+    text = ''.join(text)
+    print(get_occurrences(patt, text))
+
+
 
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
+    #test()
