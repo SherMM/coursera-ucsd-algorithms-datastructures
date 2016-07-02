dbb5ad722f85e35742f790a3470bc898315ecc9d
Shermm <helioblast86@gmail.com>
2016-07-02 13:38:41 -0500

    finish week4 of graphs

--- Graphs/Week4/negative_cycle/negative_cycle.py
+++ Graphs/Week4/negative_cycle/negative_cycle.py
@@ -1,10 +1,46 @@
 #Uses python3
 
 import sys
+from collections import deque
 
 
-def negative_cycle(adj, cost):
-    #write your code here
+def negative_cycle(graph, cost):
+    for node in graph:
+        is_cycle = has_cycle(graph, cost, node)
+        if is_cycle:
+            return 1
+    return 0
+
+
+
+def has_cycle(graph, cost, s, infinity=float("inf")):
+    dists, prev, count, seen = [], [], [], []
+    for _ in range(len(graph)):
+        dists.append(infinity)
+        prev.append(None)
+        count.append(0)
+        seen.append(False)
+    dists[s] = 0
+    seen[s] = True
+    
+    queue = deque()
+    queue.appendleft(s)
+    while queue:
+        curr = queue.pop()
+        seen[curr] = False
+        count[curr] += 1
+
+        # cycle detected
+        if count[curr] >= len(graph):
+            return 1
+
+        for node in graph[curr]:
+            if dists[node] > dists[curr] + cost[(curr, node)]:
+                dists[node] = dists[curr] + cost[(curr, node)]
+                prev[node] = curr
+                if not seen[node]:
+                    queue.appendleft(node)
+                    seen[node] = True
     return 0
 
 
@@ -15,9 +51,9 @@ if __name__ == '__main__':
     data = data[2:]
     edges = list(zip(zip(data[0:(3 * m):3], data[1:(3 * m):3]), data[2:(3 * m):3]))
     data = data[3 * m:]
-    adj = [[] for _ in range(n)]
-    cost = [[] for _ in range(n)]
+    graph = {i: [] for i in range(n)}
+    cost = {}
     for ((a, b), w) in edges:
-        adj[a - 1].append(b - 1)
-        cost[a - 1].append(w)
-    print(negative_cycle(adj, cost))
+        graph[a - 1].append(b - 1)
+        cost[(a-1, b-1)] = w
+    print(negative_cycle(graph, cost))
